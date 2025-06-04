df6894f7f7cbff5da63f9327bf3449a77e20fa3b
om-arya <om.arya0577@gmail.com>
2025-06-04 18:13:05 -0400

    Set up ACI frontend with Electron

---
+++ Accessible-Chart-Interpreter/frontend/main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron/main');
+
+// https://www.electronjs.org/docs/latest/tutorial/tutorial-first-app
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
