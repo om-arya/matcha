9816735e5f3feccc92f8c5fecd4ae5003891390c
om-arya <om.arya0577@gmail.com>
2025-06-18 19:32:28 -0400

    Add WCAG references to RA rules

--- Redesign-Assistant/src/accessibilityRules.ts
+++ Redesign-Assistant/src/accessibilityRules.ts
@@ -6,43 +6,43 @@ interface Rule {
 const rules = new Map<string, Rule>();
 
 rules.set("MISSING_TITLE", {
-    title: "Missing title",
-    description: "A missing title causes your graph's purpose to be ambiguous."
+    title: "WCAG 1.1.1: Graphs must have a title.",
+    description: "Clear labeling helps users understand the chart's purpose."
 });
 
 rules.set("MISSING_XLABEL", {
-    title: "Missing x-label",
-    description: "A missing x-label causes your graph's dependent variable to be unknown."
+    title: "WCAG 1.1.1: Graphs must have an x-label.",
+    description: "Clear labeling helps users understand the chart's purpose."
 });
 
 rules.set("MISSING_YLABEL", {
-    title: "Missing y-label",
-    description: "A missing y-label causes your graph's independent variable to be unknown."
+    title: "WCAG 1.1.1: Graphs must have a y-label.",
+    description: "Clear labeling helps users understand the chart's purpose."
 });
 
 rules.set("MISSING_LEGEND", {
-    title: "Missing legend (plotting multiple data series)",
-    description: "A missing legend causes ambiguity in what your graph's data series represent."
+    title: "WCAG 1.4.1: Graphs must have a legend when plotting multiple data series.",
+    description: "Descriptive legends help distinguish groups, especially when color alone isn't sufficient."
+});
+
+rules.set("INSUFFICIENT_COLOR_CONTRAST", {
+    title: "WCAG 1.4.3: Colors used must be of high contrast.",
+    description: "Users with visual impairments benefit from clear visual separation."
 });
 
 rules.set("FONTSIZE_TOO_SMALL", {
-    title: "Text size under 15 pt",
+    title: "WCAG 1.4.3: Text size must be at least 15 pt.",
     description: "Small text can cause visual strain."
 });
 
 rules.set("FIGSIZE_TOO_SMALL", {
-    title: "'figsize' under (8,5).",
+    title: "WCAG 1.4.4: 'figsize' must be at least (8,5).",
     description: "Small figures can cause labels/titles to become illegible when zoomed or magnified."
 });
 
-rules.set("INSUFFICIENT_COLOR_CONTRAST", {
-    title: "Low-contrast color combinations",
-    description: "Low-contrast color combinations can cause visual strain or confusion for colorblind readers. Use contrast checkers or WCAG-compliant palettes."
-});
-
 rules.set("ANIMATIONS", {
-    title: "Animations",
-    description: "Do not use .animation features or blinking elements in static exports. Animations can cause cognitive and sensory overload, and disorientation or seizures in sensitive readers."
+    title: "WCAG 2.3.1: Avoid animations.",
+    description: "Do not use .animation features or blinking elements in static exports. Animations can cause cognitive and sensory overload, and disorientation or seizures in sensitive users."
 });
 
 export { rules };
