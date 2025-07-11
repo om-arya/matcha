e4565a0dc5330c3f14f08056e752a5a1c5d01c35
om-arya <om.arya0577@gmail.com>
2025-07-10 20:42:38 -0400

    Implement RA flaw fixing and injection functionalities

--- services/redesign_service/redesign_service.py
+++ services/redesign_service/redesign_service.py
@@ -19,16 +19,15 @@ app.add_middleware(
 genai.configure(api_key=GEMINI_API_KEY)
 client = genai.GenerativeModel('gemini-2.5-flash')
 
-@app.get("/find_accessibility_flaws")
-def find_accessibility_flaws(mpl_file: str):
-    pass
+def generate_response(prompt: str):
+    result = client.generate_content(prompt)
+    candidate = result.candidates[0]
+    return candidate.content.parts[0].text.strip()
 
-@app.get("/find_misleading_visual_flaws")
-def find_misleading_visual_flaws(mpl_file: str):
-    pass
+@app.get("/find_flaws")
+def find_flaws(mpl_file: str):
+    # Insert code from GS
 
-@app.get("/find_misleading_contextual_flaws")
-def find_misleading_contextual_flaws(mpl_file: str):
     prompt = f"""
         I will present a list of rule codes mapped to their descriptions, followed by a Matplotlib file.
         Output a list of the rule codes the Matplotlib file violates in the format "RULE_CODE1, RULE_CODE2, ...".
@@ -41,15 +40,48 @@ def find_misleading_contextual_flaws(mpl_file: str):
         Matplotlib file:
         {mpl_file}
     """
-    response = client.generate_content(prompt)
-    candidate = response.candidates[0]
-    return candidate.content.parts[0].text.strip()
+    response = generate_response(prompt)
+    return response
+
+@app.get("/find_and_fix_flaws")
+def find_and_fix_flaws(mpl_file: str):
+    flaws = find_flaws(mpl_file)
+
+    prompt = f"""
+        I will present a list of flaws, followed by a Matplotlib file containing those flaws.
+        Output the Matplotlib file with the flaws removed.
+
+        Flaws: {flaws}
+
+        Matplotlib file:
+        {mpl_file}
+    """
+
+    response = generate_response(prompt)
+    return response
 
 @app.get("/insert_flaws")
 def insert_flaws(mpl_file: str, flaws: list[str]):
-    pass
+    existing_flaws = find_flaws(mpl_file)
+    new_flaws = []
+    for flaw in flaws:
+        if flaw not in existing_flaws:
+            new_flaws.append(flaw)
+
+    prompt = f"""
+        I will present a list of flaws, followed by a Matpotlib file.
+        Output the Matplotlib file with the flaws inserted into it.
+        
+        Flaws: {new_flaws}
+
+        Matplotlib file:
+        {mpl_file}
+    """
+
+    response = generate_response(prompt)
+    return response
 
 if __name__ == "__main__":
-    print(find_misleading_contextual_flaws("Hello"))
+    print(find_and_fix_flaws("Hello"))
     import uvicorn
     uvicorn.run("redesign_service:app", host="127.0.0.1", port=8001, reload=True)
